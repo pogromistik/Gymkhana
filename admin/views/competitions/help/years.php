2b5dfc31660a59e213899a9ee82702ccdc041b11
nadia <nadia__@bk.ru>
2017-03-04 17:27:20 +0500

    add years

---
+++ admin/views/competitions/help/years.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use common\models\Year;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\search\YearSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Года';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="years-index">
+
+	<p>
+		<?= Html::a('Добавить год', ['/competitions/help/year-view'], ['class' => 'btn btn-success']) ?>
+	</p>
+	<?= GridView::widget([
+		'dataProvider' => $dataProvider,
+		'filterModel'  => $searchModel,
+		'columns'      => [
+			['class' => 'yii\grid\SerialColumn'],
+
+			[
+				'attribute' => 'year',
+				'format'    => 'raw',
+				'value'     => function (Year $year) {
+					return Html::a($year->year, ['/competitions/help/year-view', 'yearId' => $year->id] );
+				}
+			],
+			[
+				'attribute' => 'status',
+				'format'    => 'raw',
+				'value'     => function (Year $year) {
+					return Year::$statusesTitle[$year->status];
+				}
+			]
+		],
+	]); ?>
+</div>
