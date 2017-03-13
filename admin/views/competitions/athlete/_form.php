5db1279bf313f96890e48048597adb7c59004989
nadia <nadia__@bk.ru>
2017-03-13 21:24:55 +0500

    add text

--- admin/views/competitions/athlete/_form.php
+++ admin/views/competitions/athlete/_form.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use yii\bootstrap\ActiveForm;
 use kartik\widgets\Select2;
 
 /* @var $this yii\web\View */
@@ -27,7 +27,17 @@ use kartik\widgets\Select2;
 	
 	<?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 	
-	<?= $form->field($model, 'athleteClassId')->dropDownList(\yii\helpers\ArrayHelper::map(
+	<?php
+	$startClass = \common\models\AthletesClass::getStartClass();
+	$text = '';
+	if ($startClass) {
+		$text = 'По умолчанию будет установлен класс ' . $startClass->title;
+	}
+	?>
+	<?= $form->field($model, 'athleteClassId',
+		['inputTemplate' => '<div class="input-with-description"><div class="text">
+ ' . $text . '
+</div>{input}</div>'])->dropDownList(\yii\helpers\ArrayHelper::map(
 		\common\models\AthletesClass::find()->andWhere(['status' => \common\models\AthletesClass::STATUS_ACTIVE])->orderBy(['sort' => SORT_ASC])->all(), 'id', 'title'
 	), ['prompt' => 'Укажите класс спортсмена']) ?>
 	
