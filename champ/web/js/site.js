16677951d3bf6c8827121b5ac4368cf49b813596
nadia <nadia__@bk.ru>
2017-03-19 11:32:47 +0500

    add scroll

--- champ/web/js/site.js
+++ champ/web/js/site.js
@@ -112,6 +112,7 @@ $(document).on("submit", '.registrationAthlete', function (e) {
         type: "POST",
         data: form.serialize(),
         success: function (result) {
+            $('html, body').animate({ scrollTop: $('.modal-footer').offset().top }, 500);
             if (result == true) {
                 form.find('.alert-success').text('Ваша заявка успешно отправлена. Пароль для доступа в личный кабинет будет' +
                     'отправлен на указанную почту в течение 24 часов. Если этого не произойдёт - пожалуйста, сообщите нам.').show();
