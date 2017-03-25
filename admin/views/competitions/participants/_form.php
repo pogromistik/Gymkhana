c12f1d1fa89805e4e2a068157aea7bb20211234e
nadia <nadia__@bk.ru>
2017-03-25 11:12:48 +0500

    fix

--- admin/views/competitions/participants/_form.php
+++ admin/views/competitions/participants/_form.php
@@ -13,23 +13,23 @@ use common\models\AthletesClass;
 ?>
 
 <div class="participant-form">
-
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'championshipId')->hiddenInput()->label(false)->error(false) ?>
-
-    <?= $form->field($model, 'stageId')->hiddenInput()->label(false)->error(false) ?>
-
-    <?= $form->field($model, 'athleteId')->widget(Select2::classname(), [
-	    'name'    => 'kv-type-01',
-	    'data'    => ArrayHelper::map(\common\models\Athlete::getActiveAthletes(), 'id', function(\common\models\Athlete $item) {
-	        return $item->lastName . ' ' . $item->firstName;
-        }),
-	    'options' => [
-		    'placeholder' => 'Выберите спортсмена...',
-		    'id' => 'athlete-id',
-	    ],
-    ]) ?>
+	
+	<?php $form = ActiveForm::begin(); ?>
+	
+	<?= $form->field($model, 'championshipId')->hiddenInput()->label(false)->error(false) ?>
+	
+	<?= $form->field($model, 'stageId')->hiddenInput()->label(false)->error(false) ?>
+	
+	<?= $form->field($model, 'athleteId')->widget(Select2::classname(), [
+		'name'    => 'kv-type-01',
+		'data'    => ArrayHelper::map(\common\models\Athlete::getActiveAthletes(), 'id', function (\common\models\Athlete $item) {
+			return $item->lastName . ' ' . $item->firstName . '(' . $item->city->title . ')';
+		}),
+		'options' => [
+			'placeholder' => 'Выберите спортсмена...',
+			'id'          => 'athlete-id',
+		],
+	]) ?>
 	
 	<?= $form->field($model, 'motorcycleId')->widget(\kartik\widgets\DepDrop::className(), [
 		'options'       => ['id' => 'motorcycle-id'],
@@ -39,19 +39,19 @@ use common\models\AthletesClass;
 			'url'         => \yii\helpers\Url::to('/competitions/participants/motorcycle-category')
 		]
 	]) ?>
-
-    <?= $form->field($model, 'internalClassId')->dropDownList(
-            ArrayHelper::map(InternalClass::getActiveClasses($model->championshipId), 'id', 'title'),
-        ['prompt' => 'Выберите класс награждения']) ?>
-
-    <?= $form->field($model, 'number')->textInput() ?>
-
-    <?= $form->field($model, 'sort')->textInput() ?>
+	
+	<?= $form->field($model, 'internalClassId')->dropDownList(
+		ArrayHelper::map(InternalClass::getActiveClasses($model->championshipId), 'id', 'title'),
+		['prompt' => 'Выберите класс награждения']) ?>
+	
+	<?= $form->field($model, 'number')->textInput() ?>
+	
+	<?= $form->field($model, 'sort')->textInput() ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Добавить' : 'Сохранить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+		<?= Html::submitButton($model->isNewRecord ? 'Добавить' : 'Сохранить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
-
-    <?php ActiveForm::end(); ?>
+	
+	<?php ActiveForm::end(); ?>
 
 </div>
