262cf164c1263b08500376e0b68d557bd59c938d
nadia <nadia__@bk.ru>
2017-03-31 17:58:02 +0500

    fix footer

--- champ/views/layouts/_footer.php
+++ champ/views/layouts/_footer.php
@@ -2,10 +2,10 @@
 	<div class="container-fluid">
         <div class="row">
             <div class="col-md-6 col-xs-12 text-left">
-                <a href="https://vk.com/id19792817" target="_blank">Сайт разработан чудесным человеком</a>
+                <a href="#" data-toggle="modal" data-target="#feedbackForm">Обратная связь</a>
             </div>
             <div class="col-md-6 col-xs-12 text-right">
-                <a href="#" data-toggle="modal" data-target="#feedbackForm">Обратная связь</a>
+                <a href="https://vk.com/id19792817" target="_blank">Сайт разработан чудесным человеком</a>
             </div>
         </div>
 	</div>
