a90344956dc12054e3d4c801627291d8585d19cd
nadia <nadia__@bk.ru>
2017-04-03 20:10:17 +0500

    add internal classes to stages

--- champ/views/competitions/stage.php
+++ champ/views/competitions/stage.php
@@ -12,150 +12,157 @@ use yii\bootstrap\Html;
 $time = time();
 $city = $stage->city;
 if ($city->timezone) {
-    $timezone = '(' . $city->title .', UTC ' . $city->utc . ')';
+	$timezone = '(' . $city->title . ', UTC ' . $city->utc . ')';
 } else {
-    $timezone = '(Москва, UTC +3)';
+	$timezone = '(Москва, UTC +3)';
 }
 
+$championship = $stage->championship;
 ?>
 
-<div class="row">
-    <div class="col-bg-8 col-lg-9 col-md-10 col-sm-12">
-        <div class="title-with-bg">
-			<?= $stage->championship->title ?>
-        </div>
+    <div class="row">
+        <div class="col-bg-8 col-lg-9 col-md-10 col-sm-12">
+            <div class="title-with-bg">
+				<?= $championship->title ?>
+            </div>
 
-        <div class="pl-10">
-            <h4><?= $stage->title ?>
-                , <?= $stage->city->title ?> <?php if ($stage->dateOfThe) { ?>, <?= $stage->dateOfTheHuman ?><?php } ?>
-                <span class="label label-success"><?= Stage::$statusesTitle[$stage->status] ?></span></h4>
-			<?php if ($stage->description) { ?>
-                <p><?= $stage->description ?></p>
-			<?php } ?>
-			<?php if ($stage->status == Stage::STATUS_UPCOMING || $stage->status == Stage::STATUS_START_REGISTRATION) { ?>
-				<?php if ($stage->startRegistration) { ?>
-                    <p>
-                        Начало регистрации: <?= $stage->startRegistrationHuman ?> <?= $timezone ?>
-						<?php if ($stage->endRegistration) { ?>
-                            <br>
-                            Завершение регистрации: <?= $stage->endRegistrationHuman ?> <?= $timezone ?>
-						<?php } ?>
-                    </p>
-				<?php } else { ?>
-                    <p>Регистрация на этап завершена</p>
+            <div class="pl-10">
+                <h4><?= $stage->title ?>
+                    , <?= $stage->city->title ?> <?php if ($stage->dateOfThe) { ?>, <?= $stage->dateOfTheHuman ?><?php } ?>
+                    <span class="label label-success"><?= Stage::$statusesTitle[$stage->status] ?></span></h4>
+				<?php if ($stage->description) { ?>
+                    <p><?= $stage->description ?></p>
 				<?php } ?>
-			<?php } ?>
-			<?php if ($stage->documentId) { ?>
-                <div class="regulations">
-					<?= Html::a($stage->document->title, ['/base/download', 'id' => $stage->documentId]) ?>
-                </div>
-			<?php } ?>
-            
-            <?php if ($stage->referenceTime) { ?>
-                <div>
-                    Эталонное время трассы: <?= $stage->referenceTimeHuman ?>
-                </div>
-            <?php } ?>
-			
-			<?php if ($stage->trackPhoto && $stage->trackPhotoStatus == Stage::PHOTO_PUBLISH) { ?>
-                <div class="track-photo">
-                    <div class="toggle">
-                        <div class="title">Посмотреть схему трассы</div>
-                        <div class="toggle-content">
-							<?= \yii\bootstrap\Html::img(\Yii::getAlias('@filesView') . '/' . $stage->trackPhoto) ?>
+				<?php if ($stage->status == Stage::STATUS_UPCOMING || $stage->status == Stage::STATUS_START_REGISTRATION) { ?>
+					<?php if ($stage->startRegistration) { ?>
+                        <p>
+                            Начало регистрации: <?= $stage->startRegistrationHuman ?> <?= $timezone ?>
+							<?php if ($stage->endRegistration) { ?>
+                                <br>
+                                Завершение регистрации: <?= $stage->endRegistrationHuman ?> <?= $timezone ?>
+							<?php } ?>
+                        </p>
+					<?php } else { ?>
+                        <p>Регистрация на этап завершена</p>
+					<?php } ?>
+				<?php } ?>
+				<?php if ($stage->documentId) { ?>
+                    <div class="regulations">
+						<?= Html::a($stage->document->title, ['/base/download', 'id' => $stage->documentId]) ?>
+                    </div>
+				<?php } ?>
+				
+				<?php if ($internalClassesTitle = $championship->getInternalClassesTitle()) { ?>
+                    <div>
+                        Классы награждения: <?= $internalClassesTitle ?>
+                    </div>
+				<?php } ?>
+				
+				<?php if ($stage->referenceTime) { ?>
+                    <div>
+                        Эталонное время трассы: <?= $stage->referenceTimeHuman ?>
+                    </div>
+				<?php } ?>
+				
+				<?php if ($stage->trackPhoto && $stage->trackPhotoStatus == Stage::PHOTO_PUBLISH) { ?>
+                    <div class="track-photo">
+                        <div class="toggle">
+                            <div class="title">Посмотреть схему трассы</div>
+                            <div class="toggle-content">
+								<?= \yii\bootstrap\Html::img(\Yii::getAlias('@filesView') . '/' . $stage->trackPhoto) ?>
+                            </div>
                         </div>
                     </div>
-                </div>
-			<?php } ?>
-			
-			<?php if ($stage->startRegistration && $time >= $stage->startRegistration
-				&& (!$stage->endRegistration || $time <= $stage->endRegistration) && $stage->status != Stage::STATUS_PAST
-			) { ?>
-                <div class="pt-30">
-					<?php if (\Yii::$app->user->isGuest) { ?>
-                        <a href="#" class="btn btn-dark" data-toggle="modal" data-target="#enrollForm">Зарегистрироваться</a>
-					<?php } else { ?>
-                        <a href="#" class="btn btn-dark" data-toggle="modal" data-target="#enrollAuthorizedForm">Зарегистрироваться</a>
-					<?php } ?>
-                </div>
-			<?php } ?>
-			
-			<?php if ($time >= $stage->startRegistration || $stage->status != Stage::STATUS_UPCOMING) { ?>
-
-                <div class="results pt-20">
-                    <div class="pb-10">
-						<?= \yii\bootstrap\Html::a('Скачать в xls', \yii\helpers\Url::to([
-							'/export/export',
-							'modelId' => $stage->id,
-							'type'    => \champ\controllers\ExportController::TYPE_STAGE
-						]), ['class' => 'btn btn-light']) ?>
+				<?php } ?>
+				
+				<?php if ($stage->startRegistration && $time >= $stage->startRegistration
+					&& (!$stage->endRegistration || $time <= $stage->endRegistration) && $stage->status != Stage::STATUS_PAST
+				) { ?>
+                    <div class="pt-30">
+						<?php if (\Yii::$app->user->isGuest) { ?>
+                            <a href="#" class="btn btn-dark" data-toggle="modal" data-target="#enrollForm">Зарегистрироваться</a>
+						<?php } else { ?>
+                            <a href="#" class="btn btn-dark" data-toggle="modal" data-target="#enrollAuthorizedForm">Зарегистрироваться</a>
+						<?php } ?>
                     </div>
-					
-					<?php if ($participantsByInternalClasses) { ?>
-                        <div class="result-scheme active">
-                            <div class="change-type">
-                                <a href="#" class="change-result-scheme">Посмотреть результаты по классам
-                                    награждений</a>
+				<?php } ?>
+				
+				<?php if ($time >= $stage->startRegistration || $stage->status != Stage::STATUS_UPCOMING) { ?>
+
+                    <div class="results pt-20">
+                        <div class="pb-10">
+							<?= \yii\bootstrap\Html::a('Скачать в xls', \yii\helpers\Url::to([
+								'/export/export',
+								'modelId' => $stage->id,
+								'type'    => \champ\controllers\ExportController::TYPE_STAGE
+							]), ['class' => 'btn btn-light']) ?>
+                        </div>
+						
+						<?php if ($participantsByInternalClasses) { ?>
+                            <div class="result-scheme active">
+                                <div class="change-type">
+                                    <a href="#" class="change-result-scheme">Посмотреть результаты по классам
+                                        награждений</a>
+                                </div>
+                                <div class="text-right">
+									<?php if ($sortBy) { ?>
+										<?= Html::a('отсортировать по местам в классе', ['stage', 'id' => $stage->id]) ?>
+									<?php } else { ?>
+										<?= Html::a('отсортировать по местам вне класса', ['stage', 'id' => $stage->id, 'sortBy' => 'place']) ?>
+									<?php } ?>
+                                </div>
+								<?= $this->render('_byJapan', ['stage' => $stage, 'participants' => $participantsByJapan]) ?>
                             </div>
+                            <div class="result-scheme">
+                                <div class="change-type">
+                                    <a href="#" class="change-result-scheme">Посмотреть результаты по японской
+                                        схеме</a>
+                                </div>
+                                <div class="text-right">
+									<?php if ($sortBy) { ?>
+										<?= Html::a('отсортировать по местам в классе', ['stage', 'id' => $stage->id, 'showByClasses' => true]) ?>
+									<?php } else { ?>
+										<?= Html::a('отсортировать по местам вне класса', ['stage', 'id' => $stage->id, 'sortBy' => 'place', 'showByClasses' => true]) ?>
+									<?php } ?>
+                                </div>
+								<?= $this->render('_byInternalClasses', ['stage' => $stage, 'participants' => $participantsByInternalClasses]) ?>
+                            </div>
+						<?php } else { ?>
                             <div class="text-right">
-                                <?php if ($sortBy) { ?>
-	                                <?= Html::a('отсортировать по местам в классе', ['stage', 'id' => $stage->id]) ?>
-                                <?php } else { ?>
-								<?= Html::a('отсортировать по местам вне класса', ['stage', 'id' => $stage->id, 'sortBy' => 'place']) ?>
-                                <?php } ?>
+								<?php if ($sortBy) { ?>
+									<?= Html::a('отсортировать по местам в классе', ['stage', 'id' => $stage->id]) ?>
+								<?php } else { ?>
+									<?= Html::a('отсортировать по местам вне класса', ['stage', 'id' => $stage->id, 'sortBy' => 'place']) ?>
+								<?php } ?>
                             </div>
 							<?= $this->render('_byJapan', ['stage' => $stage, 'participants' => $participantsByJapan]) ?>
-                        </div>
-                        <div class="result-scheme">
-                            <div class="change-type">
-                                <a href="#" class="change-result-scheme">Посмотреть результаты по японской
-                                    схеме</a>
-                            </div>
-                            <div class="text-right">
-		                        <?php if ($sortBy) { ?>
-			                        <?= Html::a('отсортировать по местам в классе', ['stage', 'id' => $stage->id, 'showByClasses' => true]) ?>
-		                        <?php } else { ?>
-			                        <?= Html::a('отсортировать по местам вне класса', ['stage', 'id' => $stage->id, 'sortBy' => 'place', 'showByClasses' => true]) ?>
-		                        <?php } ?>
-                            </div>
-							<?= $this->render('_byInternalClasses', ['stage' => $stage, 'participants' => $participantsByInternalClasses]) ?>
-                        </div>
-					<?php } else { ?>
-                        <div class="text-right">
-							<?php if ($sortBy) { ?>
-								<?= Html::a('отсортировать по местам в классе', ['stage', 'id' => $stage->id]) ?>
-							<?php } else { ?>
-								<?= Html::a('отсортировать по местам вне класса', ['stage', 'id' => $stage->id, 'sortBy' => 'place']) ?>
-							<?php } ?>
-                        </div>
-						<?= $this->render('_byJapan', ['stage' => $stage, 'participants' => $participantsByJapan]) ?>
-					<?php } ?>
-                </div>
-			<?php } ?>
+						<?php } ?>
+                    </div>
+				<?php } ?>
+            </div>
+
         </div>
 
-    </div>
-
-    <div class="col-bg-4 col-lg-3 col-md-2 col-sm-12 list-nav">
-		<?php
-		$stages = $stage->championship->stages;
-		if ($stages) {
-			?>
-            <ul>
-				<?php foreach ($stages as $item) { ?>
-                    <li>
-						<?= Html::a($item->title, ['/competitions/stage', 'id' => $item->id]) ?>
-                    </li>
-				<?php } ?>
-                <li>
-	                <?= Html::a('Итоги чемпионата', ['/competitions/championship-result', 'championshipId' => $stage->championshipId]) ?>
-                </li>
-            </ul>
+        <div class="col-bg-4 col-lg-3 col-md-2 col-sm-12 list-nav">
 			<?php
-		}
-		?>
+			$stages = $stage->championship->stages;
+			if ($stages) {
+				?>
+                <ul>
+					<?php foreach ($stages as $item) { ?>
+                        <li>
+							<?= Html::a($item->title, ['/competitions/stage', 'id' => $item->id]) ?>
+                        </li>
+					<?php } ?>
+                    <li>
+						<?= Html::a('Итоги чемпионата', ['/competitions/championship-result', 'championshipId' => $stage->championshipId]) ?>
+                    </li>
+                </ul>
+				<?php
+			}
+			?>
+        </div>
     </div>
-</div>
 
 <?php if (\Yii::$app->user->isGuest) { ?>
 	<?= $this->render('_enrollForm', ['stage' => $stage]) ?>
