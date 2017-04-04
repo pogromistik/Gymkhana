0a0bba0a70b3ca4889f4cc4421f1d2c45692afb2
nadia <nadia__@bk.ru>
2017-04-04 10:33:26 +0500

    add google analytics

--- site/views/layouts/album.php
+++ site/views/layouts/album.php
@@ -53,6 +53,24 @@ AlbumAsset::register($this);
 </script>
 <noscript><div><img src="https://mc.yandex.ru/watch/35953055" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
+
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-87412253-2', 'auto');
+    ga('send', 'pageview');
+
+</script>
 <?php $this->endBody() ?>
 </body>
 </html>
