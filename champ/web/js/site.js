79b4444b8504ef31402d33778a6a9d0e10214561
nadia <nadia__@bk.ru>
2017-04-09 19:56:04 +0500

    fix menu

--- champ/web/js/site.js
+++ champ/web/js/site.js
@@ -82,7 +82,7 @@ function initAffixCheck() {
     "use strict";
     var e = $(".header");
     var windowT = $(window);
-    e.affix({offset: {top: 1}}), windowT.width() < 1025 && (windowT.off(".affix"), e.removeData("bs.affix").removeClass("affix affix-top affix-bottom"))
+    e.affix({offset: {top: 1}}), windowT.width() < 768 && (windowT.off(".affix"), e.removeData("bs.affix").removeClass("affix affix-top affix-bottom"))
 }
 
 $(document).ready(function () {
