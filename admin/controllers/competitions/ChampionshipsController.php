da72aece85f91f86c31be94b94968bd0eaac81ba
nadia <nadia__@bk.ru>
2017-04-11 20:49:20 +0500

    access for championships

--- admin/controllers/competitions/ChampionshipsController.php
+++ admin/controllers/competitions/ChampionshipsController.php
@@ -11,6 +11,7 @@ use dosamigos\editable\EditableAction;
 use Yii;
 use common\models\Championship;
 use common\models\search\ChampionshipSearch;
+use yii\web\ForbiddenHttpException;
 use yii\web\NotFoundHttpException;
 
 /**
@@ -44,6 +45,12 @@ class ChampionshipsController extends BaseController
 		$searchModel = new ChampionshipSearch();
 		$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 		$dataProvider->query->andWhere(['groupId' => $groupId]);
+		if (!\Yii::$app->user->can('globalWorkWithCompetitions')) {
+			$dataProvider->query->andWhere(['or',
+				['regionId' => null],
+				['regionId' => \Yii::$app->user->identity->regionId]
+			]);
+		}
 		
 		return $this->render('index', [
 			'searchModel'  => $searchModel,
@@ -70,7 +77,7 @@ class ChampionshipsController extends BaseController
 	
 	public function actionCreate($groupId)
 	{
-		$this->can('competitions');
+		$this->can('projectAdmin');
 		
 		$model = new Championship();
 		$model->groupId = $groupId;
@@ -87,9 +94,14 @@ class ChampionshipsController extends BaseController
 	
 	public function actionUpdate($id, $success = false)
 	{
-		$this->can('competitions');
+		$this->can('projectAdmin');
 		
 		$model = $this->findModel($id);
+		
+		if (!\Yii::$app->user->can('globalWorkWithCompetitions') && $model->regionId
+			&& $model->regionId != \Yii::$app->user->identity->regionId) {
+			throw new ForbiddenHttpException('Доступ запрещён');
+		}
 		
 		if ($model->load(Yii::$app->request->post()) && $model->save()) {
 			return $this->redirect(['update', 'id' => $model->id, 'success' => true]);
@@ -102,23 +114,6 @@ class ChampionshipsController extends BaseController
 	}
 	
 	/**
-	 * Deletes an existing Championship model.
-	 * If deletion is successful, the browser will be redirected to the 'index' page.
-	 *
-	 * @param integer $id
-	 *
-	 * @return mixed
-	 */
-	public function actionDelete($id)
-	{
-		$this->can('competitions');
-		
-		$this->findModel($id)->delete();
-		
-		return $this->redirect(['index']);
-	}
-	
-	/**
 	 * Finds the Championship model based on its primary key value.
 	 * If the model is not found, a 404 HTTP exception will be thrown.
 	 *
@@ -126,12 +121,17 @@ class ChampionshipsController extends BaseController
 	 *
 	 * @return Championship the loaded model
 	 * @throws NotFoundHttpException if the model cannot be found
+	 * @throws ForbiddenHttpException if access denied
 	 */
 	protected function findModel($id)
 	{
 		$this->can('competitions');
 		
 		if (($model = Championship::findOne($id)) !== null) {
+			if (!\Yii::$app->user->can('globalWorkWithCompetitions') && $model->regionId
+				&& $model->regionId != \Yii::$app->user->identity->regionId) {
+				throw new ForbiddenHttpException('Доступ запрещён');
+			}
 			return $model;
 		} else {
 			throw new NotFoundHttpException('The requested page does not exist.');
@@ -140,10 +140,11 @@ class ChampionshipsController extends BaseController
 	
 	public function actionAddGroup()
 	{
-		$this->can('competitions');
+		$this->can('projectAdmin');
 		
 		$group = new RegionalGroup();
-		if ($group->load(\Yii::$app->request->post()) && $group->save()) {
+		if ($group->load(\Yii::$app->request->post()) && $group->validate()) {
+			$group->save(false);
 			return true;
 		} else {
 			return 'Возникла ошибка при добавлении группы';
@@ -152,10 +153,15 @@ class ChampionshipsController extends BaseController
 	
 	public function actionAddClass()
 	{
-		$this->can('competitions');
+		$this->can('projectAdmin');
 		
 		$class = new InternalClass();
-		if ($class->load(\Yii::$app->request->post()) && $class->save()) {
+		if ($class->load(\Yii::$app->request->post()) && $class->validate()) {
+			$championship = Championship::findOne($class->championshipId);
+			if ($championship->regionId && $championship->regionId != \Yii::$app->user->identity->regionId) {
+				return 'Доступ запрещен';
+			}
+			$class->save();
 			return true;
 		}
 		
@@ -164,7 +170,7 @@ class ChampionshipsController extends BaseController
 	
 	public function actionChangeClassStatus($id, $status)
 	{
-		$this->can('competitions');
+		$this->can('projectAdmin');
 		
 		$class = InternalClass::findOne($id);
 		if (!$class) {
@@ -172,6 +178,10 @@ class ChampionshipsController extends BaseController
 		}
 		if (!array_key_exists($status, InternalClass::$statusesTitle)) {
 			return 'Статус не существует';
+		}
+		$championship = Championship::findOne($class->championshipId);
+		if ($championship->regionId && $championship->regionId != \Yii::$app->user->identity->regionId) {
+			return 'Доступ запрещен';
 		}
 		$class->status = $status;
 		if ($class->save()) {
--- admin/views/competitions/championships/view.php
+++ admin/views/competitions/championships/view.php
@@ -13,12 +13,13 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="championship-view">
     <p>
-		<?= Html::a('Редактировать', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-		<?= Html::a('Добавить этап', ['/competitions/stages/create', 'championshipId' => $model->id], [
-			'class' => 'btn btn-success',
-			'title' => 'Просмотр'
-		]); ?>
-		<?=  Html::a('Результаты', ['/competitions/championships/results', 'championshipId' => $model->id], [
+		<?php if (\Yii::$app->user->can('projectAdmin')) { ?>
+			<?= Html::a('Редактировать', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+			<?= Html::a('Добавить этап', ['/competitions/stages/create', 'championshipId' => $model->id], [
+				'class' => 'btn btn-success'
+			]); ?>
+		<?php } ?>
+		<?= Html::a('Результаты', ['/competitions/championships/results', 'championshipId' => $model->id], [
 			'class' => 'btn btn-default'
 		]); ?>
     </p>
@@ -67,9 +68,10 @@ $this->params['breadcrumbs'][] = $this->title;
             <th>Класс соревнования</th>
         </tr>
         </thead>
+		<?php $view = \Yii::$app->user->can('projectAdmin') ? 'update' : 'view'; ?>
 		<?php foreach ($model->stages as $stage) { ?>
             <tr>
-                <td><?= Html::a($stage->title, ['/competitions/stages/view', 'id' => $stage->id]) ?></td>
+                <td><?= Html::a($stage->title, ['/competitions/stages/' . $view, 'id' => $stage->id]) ?></td>
                 <td><?= $stage->dateOfTheHuman ?></td>
                 <td><?= $stage->startRegistrationHuman ?></td>
                 <td><?= $stage->endRegistrationHuman ?></td>
@@ -79,10 +81,24 @@ $this->params['breadcrumbs'][] = $this->title;
 		<?php } ?>
     </table>
 	
-	<?php if ($model->internalClasses) { ?>
-        <h3>Классы награждения</h3>
-		<?= $this->render('_classes', [
-			'model' => $model,
-		]) ?>
+	<?php if (\Yii::$app->user->can('projectAdmin')) { ?>
+		<?php if ($model->internalClasses) { ?>
+            <h3>Классы награждения</h3>
+			<?= $this->render('_classes', [
+				'model' => $model,
+			]) ?>
+		<?php } ?>
+	<?php } else { ?>
+		<?php if ($model->activeInternalClasses) { ?>
+            <h3>Классы награждения</h3>
+            <table class="table">
+				<?php foreach ($model->activeInternalClasses as $class) { ?>
+                    <tr>
+                        <td><?= $class->title ?></td>
+                        <td><?= $class->description ?></td>
+                    </tr>
+				<?php } ?>
+            </table>
+		<?php } ?>
 	<?php } ?>
 </div>
