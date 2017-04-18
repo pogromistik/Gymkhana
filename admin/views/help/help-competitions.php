e65d428a2c88e5737e12ab0b47f072e12708c58f
nadia <nadia__@bk.ru>
2017-04-18 22:36:11 +0500

    fixes

--- admin/views/help/help-competitions.php
+++ admin/views/help/help-competitions.php
@@ -7,19 +7,28 @@ $this->title = 'Админка сайта';
 ?>
 
 <div class="alert alert-success">
-    <b>Руководства пользователя находятся в разработке. Вскоре они появятся на этой странице</b>
+    <b>Для просмотра руководства пользователя, нажмите на ссылку ниже:</b>
+    <ul>
+		<?php if (\Yii::$app->user->can('projectOrganizer')) { ?>
+            <li><a>Руководство пользователя: организатор проекта</a></li>
+		<?php } ?>
+		<?php if (\Yii::$app->user->can('projectAdmin')) { ?>
+            <li><a>Руководство пользователя: администратор проекта</a></li>
+		<?php } ?>
+        <li><a>Руководство пользователя: судья соревнований</a></li>
+    </ul>
 </div>
 
-<div class = "fun-img">
-	<div class="row">
-		<div class="col-sm-4">
-			<img src="/img/0.jpg">
-		</div>
-		<div class="col-sm-4">
-			<img src="/img/1.jpg">
-		</div>
-		<div class="col-sm-4">
-			<img src="/img/2.jpg">
-		</div>
-	</div>
+<div class="fun-img">
+    <div class="row">
+        <div class="col-sm-4">
+            <img src="/img/0.jpg">
+        </div>
+        <div class="col-sm-4">
+            <img src="/img/1.jpg">
+        </div>
+        <div class="col-sm-4">
+            <img src="/img/2.jpg">
+        </div>
+    </div>
 </div>
