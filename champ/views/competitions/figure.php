88cf8f041c33cde6f9a83450bb40de5453c74c64
nadia <nadia__@bk.ru>
2017-06-28 09:16:26 +0500

    fix need time to stage and add need time to figures

--- champ/views/competitions/figure.php
+++ champ/views/competitions/figure.php
@@ -10,6 +10,7 @@ use yii\bootstrap\Html;
  * @var \common\models\FigureTime[] $results
  * @var \common\models\Year | null  $year
  * @var bool                        $showAll
+ * @var array                       $needTime
  */
 $time = time();
 ?>
@@ -56,54 +57,115 @@ $time = time();
                     </div>
 				<?php } ?>
             </div>
-	
-	        <?php if (\Yii::$app->user->isGuest) { ?>
+
+            <div>
+				<?php if ($needTime && $figure->useForClassesCalculate) { ?>
+                    <div>
+                        Время, необходимое для повышения класса:
+                        <table class="table">
+                            <tr>
+                                <td>
+                                    <div class="row">
+                                        <div class="col-sm-6 col-xs-12">
+                                            Класс
+                                        </div>
+                                        <div class="col-sm-6 col-xs-12">
+                                            Процент
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="row">
+                                        <div class="col-sm-6 col-xs-12">
+                                            Минимальное время
+                                        </div>
+                                        <div class="col-sm-6 col-xs-12">
+                                            Максимальное время
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+							<?php foreach ($needTime as $id => $data) {
+								$cssClass = null;
+								if (isset(\common\models\Athlete::$classesCss[mb_strtoupper($data['classModel']->title, 'UTF-8')])) {
+									$cssClass = \common\models\Athlete::$classesCss[mb_strtoupper($data['classModel']->title, 'UTF-8')];
+								}
+								?>
+                                <tr class="result-<?= $cssClass ?>">
+                                    <td>
+                                        <div class="row">
+                                            <div class="col-sm-6 col-xs-12">
+												<?= $data['classModel']->title ?>
+                                            </div>
+                                            <div class="col-sm-6 col-xs-12">
+												<?= $data['percent'] ?>%
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="row">
+                                            <div class="col-sm-6 col-xs-12">
+												<?= $data['startTime'] ?>
+                                            </div>
+                                            <div class="col-sm-6 col-xs-12">
+												<?= $data['endTime'] ?>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+							<?php } ?>
+                        </table>
+                    </div>
+				<?php } ?>
+            </div>
+			
+			<?php if (\Yii::$app->user->isGuest) { ?>
                 <div class="pb-10">Отправка результатов на сайт доступна только зарегистрированным пользователям</div>
-	        <?php } ?>
-	
-	        <?php if (!$figure->useForClassesCalculate) { ?>
+			<?php } ?>
+			
+			<?php if (!$figure->useForClassesCalculate) { ?>
                 <div class="pb-10"><b>
                         По данной фигуре не производится смена класса.
                     </b></div>
-	        <?php } ?>
+			<?php } ?>
 
-    <div class="filters">
-		<?= \yii\bootstrap\Html::beginForm('', 'post', ['id' => 'figureFilterForm']) ?>
-		<?= \yii\bootstrap\Html::hiddenInput('figureId', $figure->id) ?>
-		<?= \yii\bootstrap\Html::hiddenInput('yearId', $year ? $year->id : null) ?>
-		<?= \yii\bootstrap\Html::hiddenInput('showAll', $showAll, ['id' => 'showAll']) ?>
-        <div class="row">
-            <div class="col-md-12 pb-10-md input-with-sm-pt">
-				<?= Select2::widget([
-					'name'    => 'countryId',
-					'data'    => \common\models\Country::getAll(true),
-					'options' => [
-						'placeholder' => 'Выберите страну...',
-						'id'          => 'country-id',
-					],
-				]) ?>
-            </div>
-            <div class="col-md-6 col-sm-12 input-with-sm-pt">
-				<?= \kartik\widgets\DepDrop::widget([
-					'name'           => 'regionIds',
-					'data'           => [],
-					'options'        => ['placeholder' => 'Выберите регион ...'],
-					'type'           => \kartik\widgets\DepDrop::TYPE_SELECT2,
-					'select2Options' => ['pluginOptions' => ['allowClear' => true, 'multiple' => true]],
-					'pluginOptions'  => [
-						'depends'     => ['country-id'],
-						'url'         => \yii\helpers\Url::to(['/help/country-category', 'type' => \champ\controllers\HelpController::TYPE_REGION]),
-						'loadingText' => 'Для выбранной страны нет городов...',
-						'placeholder' => 'Выберите регион...'
-					],
-					'pluginEvents'   => [
-						'change' => 'function(e){
+            <div class="filters">
+				<?= \yii\bootstrap\Html::beginForm('', 'post', ['id' => 'figureFilterForm']) ?>
+				<?= \yii\bootstrap\Html::hiddenInput('figureId', $figure->id) ?>
+				<?= \yii\bootstrap\Html::hiddenInput('yearId', $year ? $year->id : null) ?>
+				<?= \yii\bootstrap\Html::hiddenInput('showAll', $showAll, ['id' => 'showAll']) ?>
+                <div class="row">
+                    <div class="col-md-12 pb-10-md input-with-sm-pt">
+						<?= Select2::widget([
+							'name'    => 'countryId',
+							'data'    => \common\models\Country::getAll(true),
+							'options' => [
+								'placeholder' => 'Выберите страну...',
+								'id'          => 'country-id',
+							],
+						]) ?>
+                    </div>
+                    <div class="col-md-6 col-sm-12 input-with-sm-pt">
+						<?= \kartik\widgets\DepDrop::widget([
+							'name'           => 'regionIds',
+							'data'           => [],
+							'options'        => ['placeholder' => 'Выберите регион ...'],
+							'type'           => \kartik\widgets\DepDrop::TYPE_SELECT2,
+							'select2Options' => ['pluginOptions' => ['allowClear' => true, 'multiple' => true]],
+							'pluginOptions'  => [
+								'depends'     => ['country-id'],
+								'url'         => \yii\helpers\Url::to(['/help/country-category', 'type' => \champ\controllers\HelpController::TYPE_REGION]),
+								'loadingText' => 'Для выбранной страны нет городов...',
+								'placeholder' => 'Выберите регион...'
+							],
+							'pluginEvents'   => [
+								'change' => 'function(e){
 				figureFilters();
 			}',
-					]
-				]);
-				?>
-				<?php /*
+							]
+						]);
+						?>
+						<?php /*
 				<?= Select2::widget([
 					'name'          => 'regionIds',
 					'data'          => \common\models\Region::getAll(true),
@@ -121,22 +183,22 @@ $time = time();
 				]);
 				?>
  */
-				?>
-            </div>
-            <div class="col-md-6 col-sm-12 input-with-sm-pt">
-				<?= Select2::widget([
-					'name'          => 'classIds',
-					'data'          => \yii\helpers\ArrayHelper::map(\common\models\AthletesClass::find()
-						->where(['status' => \common\models\AthletesClass::STATUS_ACTIVE])
-						->orderBy(['percent' => SORT_ASC])->all(), 'id', 'title'),
-					'maintainOrder' => true,
-					'options'       => ['placeholder' => 'Выберите класс...', 'multiple' => true],
-					'pluginOptions' => [
-						'tags'               => true,
-						'maximumInputLength' => 10
-					],
-					'pluginEvents'  => [
-						'change' => 'function(e){
+						?>
+                    </div>
+                    <div class="col-md-6 col-sm-12 input-with-sm-pt">
+						<?= Select2::widget([
+							'name'          => 'classIds',
+							'data'          => \yii\helpers\ArrayHelper::map(\common\models\AthletesClass::find()
+								->where(['status' => \common\models\AthletesClass::STATUS_ACTIVE])
+								->orderBy(['percent' => SORT_ASC])->all(), 'id', 'title'),
+							'maintainOrder' => true,
+							'options'       => ['placeholder' => 'Выберите класс...', 'multiple' => true],
+							'pluginOptions' => [
+								'tags'               => true,
+								'maximumInputLength' => 10
+							],
+							'pluginEvents'  => [
+								'change' => 'function(e){
 				figureFilters();
 			}',
 							]
@@ -150,23 +212,23 @@ $time = time();
             <div class="alert alert-danger" style="display: none"></div>
 
             <div class="results pt-20">
-					<div class="row">
-                        <div class="col-sm-6 col-xs-12 pb-10">
-							<?= \yii\bootstrap\Html::a('Скачать в xls', \yii\helpers\Url::to([
-								'/export/export',
-								'modelId' => $figure->id,
-								'type'    => \champ\controllers\ExportController::TYPE_FIGURE,
-								'yearId'  => $year ? $year->id : null
-							]), ['class' => 'btn btn-light']) ?>
-                        </div>
-                        <div class="col-sm-6 col-xs-12 pb-10 text-right">
-							<?php if (!\Yii::$app->user->isGuest) { ?>
-								<?= \yii\bootstrap\Html::a('Добавить результат', \yii\helpers\Url::to([
-									'/figures/send-result',
-									'figureId' => $figure->id
-								]), ['class' => 'btn btn-dark']) ?>
-                            <?php } ?>
-                        </div>
+                <div class="row">
+                    <div class="col-sm-6 col-xs-12 pb-10">
+						<?= \yii\bootstrap\Html::a('Скачать в xls', \yii\helpers\Url::to([
+							'/export/export',
+							'modelId' => $figure->id,
+							'type'    => \champ\controllers\ExportController::TYPE_FIGURE,
+							'yearId'  => $year ? $year->id : null
+						]), ['class' => 'btn btn-light']) ?>
+                    </div>
+                    <div class="col-sm-6 col-xs-12 pb-10 text-right">
+						<?php if (!\Yii::$app->user->isGuest) { ?>
+							<?= \yii\bootstrap\Html::a('Добавить результат', \yii\helpers\Url::to([
+								'/figures/send-result',
+								'figureId' => $figure->id
+							]), ['class' => 'btn btn-dark']) ?>
+						<?php } ?>
+                    </div>
                 </div>
                 <div class="small text-right">
 					<?php $count = count($results); ?>
@@ -177,15 +239,16 @@ $time = time();
 					<?php } ?>
                 </div>
 				
-                <?php if ($figure->useForClassesCalculate) { ?>
-				<?= $this->render('_figure-result', ['results' => $results]) ?>
-                <?php } else { ?>
-	                <?= $this->render('_figure-result-without-class', ['results' => $results]) ?>
-                <?php } ?>
+				<?php if ($figure->useForClassesCalculate) { ?>
+					<?= $this->render('_figure-result', ['results' => $results]) ?>
+				<?php } else { ?>
+					<?= $this->render('_figure-result-without-class', ['results' => $results]) ?>
+				<?php } ?>
             </div>
         </div>
 
-        <a href="<?= \yii\helpers\Url::to(['/competitions/results', 'by' => \champ\controllers\CompetitionsController::RESULTS_FIGURES]) ?>">Вернуться к фигурам</a>
+        <a href="<?= \yii\helpers\Url::to(['/competitions/results', 'by' => \champ\controllers\CompetitionsController::RESULTS_FIGURES]) ?>">Вернуться
+            к фигурам</a>
     </div>
 
     <div class="col-bg-5 col-lg-3 col-md-2 col-sm-12 list-nav">
