6d552d55548d1b998f83d67c66248db82e02f68c
nadia <nadia__@bk.ru>
2017-06-29 11:11:02 +0500

    add actual percents

--- champ/views/competitions/_figure-result-without-class.php
+++ champ/views/competitions/_figure-result-without-class.php
@@ -1,40 +1,47 @@
 <?php
 /**
  * @var \common\models\FigureTime[] $results
+ * @var \common\models\Figure       $figure
  */
 ?>
 
 <?php if ($results) { ?>
-	<div class="show-pk">
-		<table class="table results">
-			<thead>
-			<tr>
-				<th><p>#</p></th>
-				<th><p>Дата</p></th>
-				<th><p>Класс</p></th>
-				<th><p>Участник</p></th>
-				<th><p>Мотоцикл</p></th>
-				<th><p>Время</p></th>
-				<th><p>Штраф</p></th>
-				<th><p>Итоговое время</p></th>
-				<th><p>Рейтинг</p></th>
-			</tr>
-			</thead>
-			<tbody>
+    <div class="show-pk">
+        <table class="table results">
+            <thead>
+            <tr>
+                <th><p>#</p></th>
+                <th><p>Дата</p></th>
+                <th><p>Класс</p></th>
+                <th><p>Участник</p></th>
+                <th><p>Мотоцикл</p></th>
+                <th><p>Время</p></th>
+                <th><p>Штраф</p></th>
+                <th><p>Итоговое время</p></th>
+	            <?php if ($figure->severalRecords) { ?>
+                    <th><p>Начальный рейтинг</p></th>
+                    <th><p>Актуальный рейтинг</p></th>
+	            <?php } else { ?>
+                    <th><p>Рейтинг</p></th>
+	            <?php } ?>
+            </tr>
+            </thead>
+            <tbody>
 			<?php
 			$place = 1;
 			foreach ($results as $item) {
 				$athlete = $item->athlete;
 				?>
-				<tr>
-					<td><?= $place++ ?></td>
-					<td><?= $item->dateForHuman ?></td>
-					<td><?= $item->athleteClassId ? $item->athleteClass->title : null ?></td>
-					<td><?= \yii\bootstrap\Html::a($athlete->getFullName(), ['/athletes/view', 'id' => $athlete->id]) ?><br><?= $athlete->city->title ?></td>
-					<td><?= $item->motorcycle->getFullTitle() ?></td>
-					<td><?= $item->timeForHuman ?></td>
-					<td><?= $item->fine ?></td>
-					<td>
+                <tr>
+                    <td><?= $place++ ?></td>
+                    <td><?= $item->dateForHuman ?></td>
+                    <td><?= $item->athleteClassId ? $item->athleteClass->title : null ?></td>
+                    <td><?= \yii\bootstrap\Html::a($athlete->getFullName(), ['/athletes/view', 'id' => $athlete->id]) ?>
+                        <br><?= $athlete->city->title ?></td>
+                    <td><?= $item->motorcycle->getFullTitle() ?></td>
+                    <td><?= $item->timeForHuman ?></td>
+                    <td><?= $item->fine ?></td>
+                    <td>
 						<?= $item->resultTimeForHuman ?>
 						<?php if ($item->recordType && $item->recordStatus == \common\models\FigureTime::NEW_RECORD_APPROVE) { ?>
 							<?= \yii\bootstrap\Html::img('/img/crown.png', [
@@ -43,73 +50,84 @@
 							]) ?>
 						<?php } ?>
 						<?php if ($item->videoLink) { ?>
-							<a href="<?= $item->videoLink ?>" target="_blank">
-								<i class="fa fa-youtube"></i>
-							</a>
+                            <a href="<?= $item->videoLink ?>" target="_blank">
+                                <i class="fa fa-youtube"></i>
+                            </a>
 						<?php } ?>
-					</td>
-					<td><?= $item->percent ?>%</td>
-				</tr>
+                    </td>
+                    <td><?= $item->percent ?>%</td>
+                    <?php if ($figure->severalRecords) { ?>
+                        <td><abbr title="<?= $item->recordInMomentHuman ?>"><?= $item->percent ?>%</abbr></td>
+                        <td><?= $item->actualPercent ?>%</td>
+                    <?php } ?>
+                </tr>
 			<?php }
 			?>
-			</tbody>
-		</table>
-	</div>
-	
-	<div class="show-mobile">
-		<table class="table results">
-			<thead>
-			<tr>
-				<th>Участник</th>
-				<th>Время</th>
-				<th>Рейтинг</th>
-			</tr>
-			</thead>
-			<tbody>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="show-mobile">
+        <table class="table results">
+            <thead>
+            <tr>
+                <th>Участник</th>
+                <th>Время</th>
+	            <?php if ($figure->severalRecords) { ?>
+                    <th>Н.рейтинг<br>А.рейтинг</th>
+	            <?php } else { ?>
+                    <th>Рейтинг</th>
+	            <?php } ?>
+            </tr>
+            </thead>
+            <tbody>
 			<?php
 			if ($results) {
 				foreach ($results as $item) {
 					$athlete = $item->athlete;
 					?>
-					<tr>
-						<td>
+                    <tr>
+                        <td>
 							<?= \yii\bootstrap\Html::a($athlete->getFullName(), ['/athletes/view', 'id' => $athlete->id]) ?>
-							<br>
-							<small>
+                            <br>
+                            <small>
 								<?= $athlete->city->title ?>
-								<br>
+                                <br>
 								<?= $item->motorcycle->getFullTitle() ?>
 								<?php if ($item->athleteClassId) { ?>
-									<br>
+                                    <br>
 									<?= $item->athleteClass->title ?>
 								<?php } ?>
-								<br>
+                                <br>
 								<?= $item->dateForHuman ?>
-							</small>
-						</td>
-						<td>
+                            </small>
+                        </td>
+                        <td>
 							<?= $item->timeForHuman ?>
 							<?php if ($item->fine) { ?>
-								<span class="red"> +<?= $item->fine ?></span>
+                                <span class="red"> +<?= $item->fine ?></span>
 							<?php } ?>
-							<br>
-							<span class="green"><?= $item->resultTimeForHuman ?></span>
+                            <br>
+                            <span class="green"><?= $item->resultTimeForHuman ?></span>
 							<?php if ($item->videoLink) { ?>
-								<br>
-								<a href="<?= $item->videoLink ?>" target="_blank">
-									<i class="fa fa-youtube"></i>
-								</a>
+                                <br>
+                                <a href="<?= $item->videoLink ?>" target="_blank">
+                                    <i class="fa fa-youtube"></i>
+                                </a>
 							<?php } ?>
-						</td>
-						<td>
+                        </td>
+                        <td>
 							<?= $item->percent ?>%
-						</td>
-					</tr>
+	                        <?php if ($figure->severalRecords) { ?>
+                                <br><?= $item->actualPercent ?>%
+	                        <?php } ?>
+                        </td>
+                    </tr>
 				<?php }
 			} ?>
-			</tbody>
-		</table>
-	</div>
+            </tbody>
+        </table>
+    </div>
 <?php } else { ?>
-	Результатов не найдено
+    Результатов не найдено
 <?php } ?>
