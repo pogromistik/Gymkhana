a1740bdedc52f5f4c5d4d13c1e75151691830fc0
nadia <nadia__@bk.ru>
2017-09-12 09:28:35 +0500

    add items for sort

--- admin/views/competitions/translate-messages/translate.php
+++ admin/views/competitions/translate-messages/translate.php
@@ -28,49 +28,50 @@ $listView = new \yii\widgets\ListView([
 ?>
 
 <div class="translate">
-	<div class="row table">
-		<div class="col-md-12">
+    <div class="row table">
+        <div class="col-md-12">
 			<?= $listView->renderSummary() ?>
-		</div>
-	</div>
-	<div>
+        </div>
+    </div>
+    <div>
 		<?= \yii\helpers\Html::beginForm(['/competitions/translate-messages/translate'], 'get') ?>
 		<?= \yii\helpers\Html::dropDownList('letter', $letter, [
-		        '{' => '{', 'А' => 'А', 'Б' => 'Б', 'В' => 'В', 'Г' => 'Г', 'Д' => 'Д', 'Е' => 'Е', 'Ё' => 'Ё', 'Ж' => 'Ж',
-			'З' =>'З', 'И' => 'И', 'К' => 'К', 'Л' => 'Л', 'М' => 'М', 'Н' => 'Н', 'О' => 'О', 'П' => 'П', 'Р' => 'Р', 'С' => 'С',
-            'Т' => 'Т', 'У' => 'У', 'Ф' => 'Ф', 'Х' => 'Х', 'Ц' => 'Ц', 'Ч' => 'Ч', 'Ш' => 'Ш', 'Щ' => 'Щ', 'Э' => 'Э', 'Ю' => 'Ю', 'Я' => 'Я'
-        ],
+			'"' => '"', '*' => '*',
+			'{' => '{', 'А' => 'А', 'Б' => 'Б', 'В' => 'В', 'Г' => 'Г', 'Д' => 'Д', 'Е' => 'Е', 'Ё' => 'Ё', 'Ж' => 'Ж',
+			'З' => 'З', 'И' => 'И', 'К' => 'К', 'Л' => 'Л', 'М' => 'М', 'Н' => 'Н', 'О' => 'О', 'П' => 'П', 'Р' => 'Р', 'С' => 'С',
+			'Т' => 'Т', 'У' => 'У', 'Ф' => 'Ф', 'Х' => 'Х', 'Ц' => 'Ц', 'Ч' => 'Ч', 'Ш' => 'Ш', 'Щ' => 'Щ', 'Э' => 'Э', 'Ю' => 'Ю', 'Я' => 'Я'
+		],
 			['prompt' => 'Выберите начальный символ', 'onchange' => 'this.form.submit()', 'class' => 'form-control']) ?>
         <button type="submit" style="visibility: hidden;" title="Сохранить"></button>
 		<?= \yii\helpers\Html::endForm() ?>
     </div>
-	<table class="table table-striped table-bordered">
+    <table class="table table-striped table-bordered">
 		<?php $form = \yii\bootstrap\ActiveForm::begin([
 			'id'                     => 'search',
 			'method'                 => 'get',
 			'enableClientValidation' => false
 		]); ?>
-		<thead>
-		<tr>
-			<th style="width: 30%">
+        <thead>
+        <tr>
+            <th style="width: 30%">
 				<?= $dataProvider->sort->link('message') ?>
 				<?= $form->field($searchModel, 'message')->textInput()->label(false) ?>
-			</th>
+            </th>
             <th style="width: 20%">Комментарий для переводчика</th>
             <!--<th>
 				Язык
 			</th>-->
-			<th>
-				Перевод
-			</th>
-			<th>
-				<button type="submit" style="visibility: hidden;" title="Сохранить"></button>
-			</th>
-		</tr>
-		</thead>
+            <th>
+                Перевод
+            </th>
+            <th>
+                <button type="submit" style="visibility: hidden;" title="Сохранить"></button>
+            </th>
+        </tr>
+        </thead>
 		
 		<?php $form->end() ?>
-		<tbody>
+        <tbody>
 		
 		
 		<?php
@@ -78,7 +79,7 @@ $listView = new \yii\widgets\ListView([
 			echo $listView->renderItem($model, $model->id, $index);
 		}
 		?>
-		</tbody>
-	</table>
+        </tbody>
+    </table>
 	<?= $listView->renderPager() ?>
 </div>
