9a3757e3c5430d0471ec3e0cd661ea71879cba96
nadia <nadia__@bk.ru>
2017-10-04 11:00:52 +0500

    install node_modules

--- admin/assets/FontAwesomeAsset.php
+++ admin/assets/FontAwesomeAsset.php
@@ -7,7 +7,7 @@ use yii\web\AssetBundle;
 
 class FontAwesomeAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/font-awesome';
+    public $sourcePath = '@bower/node_modules/font-awesome';
     public $css = [
         'css/font-awesome.min.css',
     ];
