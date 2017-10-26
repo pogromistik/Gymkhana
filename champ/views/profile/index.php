e4bb978515b73029e4aa10ce65b995f9aae3b702
nadia <nadia__@bk.ru>
2017-10-26 15:56:40 +0500

    add allowclear to countries

--- champ/views/profile/index.php
+++ champ/views/profile/index.php
@@ -48,52 +48,53 @@ use yii\web\JsExpression;
 
         <div id="about-me-link">
             <h3>Изменение фотографии</h3>
-        <div  class="help-for-athlete">
+            <div class="help-for-athlete">
                 <small>Размер загружаемого изображения не должен превышать 300КБ. Допустимые форматы: png, jpg.
                     Необходимые пропорции: 3x4 (300x400 pixels)
                 </small>
             </div>
-			<div class="pt-10">
-			<?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
-			<?php if ($athlete->photo) { ?>
-                <div class="row">
-                    <div class="col-md-2 col-sm-4 img-in-profile">
-						<?= Html::img(\Yii::getAlias('@filesView') . $athlete->photo) ?>
-                        <br>
-                        <a href="#" class="btn btn-default btn-block deletePhoto">удалить</a>
-                        <br>
+            <div class="pt-10">
+				<?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+				<?php if ($athlete->photo) { ?>
+                    <div class="row">
+                        <div class="col-md-2 col-sm-4 img-in-profile">
+							<?= Html::img(\Yii::getAlias('@filesView') . $athlete->photo) ?>
+                            <br>
+                            <a href="#" class="btn btn-default btn-block deletePhoto">удалить</a>
+                            <br>
+                        </div>
+                        <div class="col-md-10 col-sm-8">
+							<?= $form->field($athlete, 'photo')->widget(\sadovojav\cutter\Cutter::className(), [
+								'cropperOptions'        => [
+									'aspectRatio' => 3 / 4,
+								],
+								'defaultCropperOptions' => [
+									'rotatable' => false, 'zoomable' => false,
+									'movable'   => false,
+								]]) ?>
+                        </div>
                     </div>
-                    <div class="col-md-10 col-sm-8">
-						<?= $form->field($athlete, 'photo')->widget(\sadovojav\cutter\Cutter::className(), [
-							'cropperOptions'        => [
-								'aspectRatio' => 3 / 4,
-							],
-							'defaultCropperOptions' => [
-								'rotatable' => false, 'zoomable'  => false,
-								'movable'   => false,
-							]]) ?>
-                    </div>
+				<?php } else { ?>
+					<?= $form->field($athlete, 'photo')->widget(\sadovojav\cutter\Cutter::className(), [
+						'cropperOptions'        => [
+							'aspectRatio' => 3 / 4
+						],
+						'defaultCropperOptions' => [
+							'rotatable' => false, 'zoomable' => false,
+							'movable'   => false,
+						]]) ?>
+				<?php } ?>
+                <div class="form-group">
+					<?= Html::submitButton('Загрузить', ['class' => 'btn btn-primary']) ?>
                 </div>
-			<?php } else { ?>
-				<?= $form->field($athlete, 'photo')->widget(\sadovojav\cutter\Cutter::className(), [
-					'cropperOptions'        => [
-						'aspectRatio' => 3 / 4
-					],
-					'defaultCropperOptions' => [
-						'rotatable' => false, 'zoomable'  => false,
-						'movable'   => false,
-					]]) ?>
-			<?php } ?><div class="form-group">
-				<?= Html::submitButton('Загрузить', ['class' => 'btn btn-primary']) ?>
+				<?php ActiveForm::end(); ?>
             </div>
-			<?php ActiveForm::end(); ?>
-        </div>
 
-        <div class="hr-motorcycle hr-motorcycle-6"></div>
+            <div class="hr-motorcycle hr-motorcycle-6"></div>
 
-        <h3><?= \yii\bootstrap\Html::a($athlete->getFullName(), ['/athletes/view', 'id' => $athlete->id], ['target' => '_blank']) ?></h3>
-        <div class="athlete-form">
-			<?php $form = ActiveForm::begin(['enableAjaxValidation' => true, 'options' => ['id' => 'updateAthlete', 'enctype' => 'multipart/form-data']]); ?>
+            <h3><?= \yii\bootstrap\Html::a($athlete->getFullName(), ['/athletes/view', 'id' => $athlete->id], ['target' => '_blank']) ?></h3>
+            <div class="athlete-form">
+				<?php $form = ActiveForm::begin(['enableAjaxValidation' => true, 'options' => ['id' => 'updateAthlete', 'enctype' => 'multipart/form-data']]); ?>
 
                 <div class="help-for-athlete">
                     <small>Информация, обязательная для заполнения:</small>
@@ -225,63 +226,63 @@ use yii\web\JsExpression;
                     </tr>
                     </thead>
                     <tbody>
-		            <?php foreach ($motorcycles as $motorcycleInfo) { ?>
+					<?php foreach ($motorcycles as $motorcycleInfo) { ?>
                         <tr class="is-active-<?= $motorcycleInfo->status ?>">
                             <td>
-					            <?= $motorcycleInfo->getFullTitle() ?>
-					            <?php if ($motorcycleInfo->isCruiser) { ?>
+								<?= $motorcycleInfo->getFullTitle() ?>
+								<?php if ($motorcycleInfo->isCruiser) { ?>
                                     <br>
                                     <small><b>Круизёр</b></small>
-					            <?php } ?>
+								<?php } ?>
                             </td>
                             <td>
-					            <?= Editable::widget([
-						            'name'          => 'cbm',
-						            'value'         => $motorcycleInfo->cbm,
-						            'url'           => 'update-motorcycle',
-						            'type'          => 'text',
-						            'mode'          => 'inline',
-						            'clientOptions' => [
-							            'pk'        => $motorcycleInfo->id,
-							            'value'     => $motorcycleInfo->cbm,
-							            'placement' => 'right',
-						            ]
-					            ]); ?>
+								<?= Editable::widget([
+									'name'          => 'cbm',
+									'value'         => $motorcycleInfo->cbm,
+									'url'           => 'update-motorcycle',
+									'type'          => 'text',
+									'mode'          => 'inline',
+									'clientOptions' => [
+										'pk'        => $motorcycleInfo->id,
+										'value'     => $motorcycleInfo->cbm,
+										'placement' => 'right',
+									]
+								]); ?>
                             </td>
                             <td>
-					            <?= Editable::widget([
-						            'name'          => 'power',
-						            'value'         => $motorcycleInfo->power,
-						            'url'           => 'update-motorcycle',
-						            'type'          => 'text',
-						            'mode'          => 'inline',
-						            'clientOptions' => [
-							            'pk'        => $motorcycleInfo->id,
-							            'value'     => $motorcycleInfo->power,
-							            'placement' => 'right',
-						            ]
-					            ]); ?>
+								<?= Editable::widget([
+									'name'          => 'power',
+									'value'         => $motorcycleInfo->power,
+									'url'           => 'update-motorcycle',
+									'type'          => 'text',
+									'mode'          => 'inline',
+									'clientOptions' => [
+										'pk'        => $motorcycleInfo->id,
+										'value'     => $motorcycleInfo->power,
+										'placement' => 'right',
+									]
+								]); ?>
                             </td>
                             <td>
-					            <?php
-					            if ($motorcycleInfo->status) {
-						            echo Html::a('<span class="fa fa-remove"></span>', ['/competitions/motorcycles/change-status', 'id' => $motorcycleInfo->id], [
-							            'class'   => 'btn btn-danger changeMotorcycleStatus',
-							            'data-id' => $motorcycleInfo->id,
-							            'title'   => 'Удалить'
-						            ]);
-					            } else {
-						            echo Html::a('<span class="fa fa-check"></span>', ['/competitions/motorcycles/change-status',
-							            'id' => $motorcycleInfo->id], [
-							            'class'   => 'btn btn-warning changeMotorcycleStatus',
-							            'data-id' => $motorcycleInfo->id,
-							            'title'   => 'Вернуть в работу'
-						            ]);
-					            }
-					            ?>
+								<?php
+								if ($motorcycleInfo->status) {
+									echo Html::a('<span class="fa fa-remove"></span>', ['/competitions/motorcycles/change-status', 'id' => $motorcycleInfo->id], [
+										'class'   => 'btn btn-danger changeMotorcycleStatus',
+										'data-id' => $motorcycleInfo->id,
+										'title'   => 'Удалить'
+									]);
+								} else {
+									echo Html::a('<span class="fa fa-check"></span>', ['/competitions/motorcycles/change-status',
+										'id' => $motorcycleInfo->id], [
+										'class'   => 'btn btn-warning changeMotorcycleStatus',
+										'data-id' => $motorcycleInfo->id,
+										'title'   => 'Вернуть в работу'
+									]);
+								}
+								?>
                             </td>
                         </tr>
-		            <?php } ?>
+					<?php } ?>
                     </tbody>
                 </table>
             </div>
@@ -292,80 +293,86 @@ use yii\web\JsExpression;
                 <small>
                     Подписываясь на новости, вы даёте согласие на отправку писем, содержащих информацию о
                     предстоящих этапах, на ваш email (<?= $athlete->email ?>).<br>
-                    Вы можете подписаться на все новости всех регионов (просто отметив пункт "Подписаться на новостную рассылку");
-                    можете выбрать  страны и регионы, новости которых вас интересуют; можете выбрать тип новостей. (Поля для выбора
+                    Вы можете подписаться на все новости всех регионов (просто отметив пункт "Подписаться на новостную
+                    рассылку");
+                    можете выбрать страны и регионы, новости которых вас интересуют; можете выбрать тип новостей. (Поля
+                    для выбора
                     появятся после выбора пункта "Подписаться на новостную рассылку").
-                    Если вы выберите страну, но не укажите ни одного региона, вам будут приходить все новости этой страны.<br>
+                    Если вы выберите страну, но не укажите ни одного региона, вам будут приходить все новости этой
+                    страны.<br>
                     В любой момент вы можете отписаться от рассылки, сняв отметку в личном кабинете.
                 </small>
             </div>
             <div class="form pt-10">
-			    <?php $form = ActiveForm::begin(['id' => 'newslettersForm']); ?>
+				<?php $form = ActiveForm::begin(['id' => 'newslettersForm']); ?>
 
                 <div class="pb-10">
-				    <?= Html::checkbox('subscription', !$subscription->isNewRecord, [
-					    'label' => 'Подписаться на новостную рассылку',
-					    'id'    => 'subscriptionNews'
-				    ]) ?>
+					<?= Html::checkbox('subscription', !$subscription->isNewRecord, [
+						'label' => 'Подписаться на новостную рассылку',
+						'id'    => 'subscriptionNews'
+					]) ?>
                 </div>
 
                 <div class="subscription-info" style="display: <?= $subscription->isNewRecord ? 'none' : 'block' ?>">
-				    <?= $form->field($subscription, 'types')->widget(Select2::classname(), [
-						    'data'    => \common\models\NewsSubscription::$typesTitle,
-						    'options' => [
-							    'placeholder' => 'Выберите типы...',
-							    'id'          => 'subscript-types',
-							    'multiple' => true,
-						    ],
-					    ])->label('Выберите типы'); ?>
+					<?= $form->field($subscription, 'types')->widget(Select2::classname(), [
+						'data'    => \common\models\NewsSubscription::$typesTitle,
+						'options' => [
+							'placeholder' => 'Выберите типы...',
+							'id'          => 'subscript-types',
+							'multiple'    => true,
+						],
+					])->label('Выберите типы'); ?>
                     <div class="row">
                         <div class="col-md-6 col-sm-12">
-						    <?= $form->field($subscription, 'countryIds')->widget(Select2::classname(), [
-							    'data'    => Country::getAll(true),
-							    'options' => [
-								    'placeholder' => 'Выберите страну...',
-								    'id'          => 'subscript-country-id',
-								    'multiple' => true,
-							    ],
-						    ])->label('Выберите страны'); ?>
+							<?= $form->field($subscription, 'countryIds')->widget(Select2::classname(), [
+								'data'          => Country::getAll(true),
+								'options'       => [
+									'placeholder' => 'Выберите страну...',
+									'id'          => 'subscript-country-id',
+									'multiple'    => true,
+								],
+								'pluginOptions' => [
+									'allowClear' => true
+								]
+							])->label('Выберите страны'); ?>
                         </div>
                         <div class="col-md-6 col-sm-12">
-						    <?= $form->field($subscription, 'regionIds')->widget(DepDrop::classname(), [
-							    'data'           => $subscription->getRegions(true, $subscription->countryIds),
-							    'options'        => ['placeholder' => 'Выберите регионы...'],
-							    'type'           => DepDrop::TYPE_SELECT2,
-							    'select2Options' => [
-								    'pluginOptions' => [
-									    'multiple'           => true,
-									    'allowClear'         => true,
-									    'minimumInputLength' => 3,
-									    'language'           => [
-										    'errorLoading' => new JsExpression("function () { return 'Поиск результатов...'; }"),
-									    ],
-									    'ajax'               => [
-										    'url'      => \yii\helpers\Url::to(['/help/regions-list']),
-										    'dataType' => 'json',
-										    'data'     => new JsExpression('function(params) { return {title:params.term, countryId:$("#subscript-country-id").val()}; }')
-									    ],
-									    'escapeMarkup'       => new JsExpression('function (markup) { return markup; }'),
-									    'templateResult'     => new JsExpression('function(region) { return region.text; }'),
-									    'templateSelection'  => new JsExpression('function (region) { return region.text; }'),
-								    ],
-							    ],
-							    'pluginOptions'  => [
-								    'depends'     => ['subscript-country-id'],
-								    'url'         => \yii\helpers\Url::to(['/help/country-category', 'type' => \champ\controllers\HelpController::TYPE_CITY]),
-								    'loadingText' => 'Для выбранной страны нет регионов...',
-								    'placeholder' => 'Выберите регион...'
-							    ]
-						    ])->label('Выберите регионы'); ?>
+							<?= $form->field($subscription, 'regionIds')->widget(DepDrop::classname(), [
+								'data'           => $subscription->getRegions(true, $subscription->countryIds),
+								'options'        => ['placeholder' => 'Выберите регионы...'],
+								'type'           => DepDrop::TYPE_SELECT2,
+								'select2Options' => [
+									'pluginOptions' => [
+										'multiple'           => true,
+										'allowClear'         => true,
+										'minimumInputLength' => 3,
+										'language'           => [
+											'errorLoading' => new JsExpression("function () { return 'Поиск результатов...'; }"),
+										],
+										'ajax'               => [
+											'url'      => \yii\helpers\Url::to(['/help/regions-list']),
+											'dataType' => 'json',
+											'data'     => new JsExpression('function(params) { return {title:params.term, countryId:$("#subscript-country-id").val()}; }')
+										],
+										'escapeMarkup'       => new JsExpression('function (markup) { return markup; }'),
+										'templateResult'     => new JsExpression('function(region) { return region.text; }'),
+										'templateSelection'  => new JsExpression('function (region) { return region.text; }'),
+									],
+								],
+								'pluginOptions'  => [
+									'depends'     => ['subscript-country-id'],
+									'url'         => \yii\helpers\Url::to(['/help/country-category', 'type' => \champ\controllers\HelpController::TYPE_CITY]),
+									'loadingText' => 'Для выбранной страны нет регионов...',
+									'placeholder' => 'Выберите регион...'
+								]
+							])->label('Выберите регионы'); ?>
                         </div>
                     </div>
                 </div>
                 <div class="pt-10 text-right">
-				    <?= Html::submitButton('Сохранить', ['class' => 'btn btn-primary']) ?>
+					<?= Html::submitButton('Сохранить', ['class' => 'btn btn-primary']) ?>
                 </div>
-			    <?php $form->end(); ?>
+				<?php $form->end(); ?>
             </div>
         </div>
     </div>
