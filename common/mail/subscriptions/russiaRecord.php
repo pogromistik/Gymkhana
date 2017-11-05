751981548feb2efff2faf1dcda914b14cd728dfe
nadia <nadia__@bk.ru>
2017-11-05 13:33:19 +0500

    fix

--- common/mail/subscriptions/russiaRecord.php
+++ common/mail/subscriptions/russiaRecord.php
@@ -6,7 +6,7 @@
  */
 ?>
 
-<?= \Yii::t('app', 'Установлен новый Российский рекорд для фигуры {$figure}!', [
+<?= \Yii::t('app', 'Установлен новый Российский рекорд для фигуры {figure}!', [
 	'figure' => '<a href="http://gymkhana-cup.ru/competitions/figure?id=' . $model->id . '" target="_blank" style="color: #56a025">'
 		. $model->title . '</a>'
 ], $language) ?>
