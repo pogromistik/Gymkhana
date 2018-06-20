d5497cc8e1850f533145583c7ecba8f6be47295c
nadia <nadia__@bk.ru>
2018-06-21 00:04:19 +0500

    fix mail

--- common/mail/new-account.php
+++ common/mail/new-account.php
@@ -3,18 +3,26 @@
  * @var \common\models\Athlete $athlete
  * @var string                 $password
  */
+switch ($athlete->language) {
+    case \common\models\TranslateMessage::LANGUAGE_RU:
+        $hostName = 'gymkhana-cup.ru';
+        break;
+    default:
+	    $hostName = 'gymkhana-cup.com';
+}
 ?>
 
 <?= \Yii::t('app', 'Ваша регистрация на сайте {site} подтверждена.',
-    ['site' => '<a href="http://gymkhana-cup.ru/" target="_blank">gymkhana-cup.ru</a>']) ?>
+	['site' => '<a href="https://' . $hostName . '/" target="_blank">' . $hostName . '</a>'], $athlete->language) ?>
 <br>
-<?= \Yii::t('app', 'Данные для входа в личный кабинет:') ?><br>
-<b><?= \Yii::t('app', 'Логин') ?>:</b> <?= $athlete->login ?> <?= \Yii::t('app', 'или') ?> <?= $athlete->email ?><br>
-<b><?= \Yii::t('app', 'Пароль') ?>:</b> <?= $password ?><br>
-<?= \Yii::t('app', 'Ссылка на вход в личный кабинет') ?>: <a href="http://gymkhana-cup.ru/site/login/" target="_blank">gymkhana-cup.ru/site/login/</a>.<br>
+<?= \Yii::t('app', 'Данные для входа в личный кабинет:', [], $athlete->language) ?><br>
+<b><?= \Yii::t('app', 'Логин', [], $athlete->language) ?>:</b> <?= $athlete->login ?> <?= \Yii::t('app', 'или', [], $athlete->language) ?> <?= $athlete->email ?><br>
+<b><?= \Yii::t('app', 'Пароль', [], $athlete->language) ?>:</b> <?= $password ?><br>
+<?= \Yii::t('app', 'Ссылка на вход в личный кабинет', [], $athlete->language) ?>: <a href="https://<?= $hostName ?>/site/login/"
+                                                             target="_blank"><?= $hostName ?>/site/login/</a>.
 <br>
-<?= \Yii::t('app', 'Пожалуйста, проверьте данные профиля в своём кабинете. В случае, если они слишком далеки от реальности - обязательно свяжитесь с нами') ?>:
+<br><?= \Yii::t('app', 'Пожалуйста, проверьте данные профиля в своём кабинете. В случае, если они слишком далеки от реальности - обязательно свяжитесь с нами', [], $athlete->language) ?>:
 <br>
 <a href="https://vk.com/famalata" target="_blank">vk.com/famalata</a><br>
 <a href="https://vk.com/id19792817" target="_blank">vk.com/id19792817</a><br>
-<?= \Yii::t('app', 'Или по почте') ?>: lyadetskaya.ns@yandex.ru
+<?= \Yii::t('app', 'Или по почте', [], $athlete->language) ?>: gymkhana.cup@gmail.com
